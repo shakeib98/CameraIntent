4d0278cb42244430172b14bf9fb0c34327653b5c
shakeib98 <shakeib98@outlook.com>
2018-10-16 01:29:27 +0500

    Initial commit

---
+++ app/src/main/java/com/example/shakeib/cameraintent/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.shakeib.cameraintent
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn_camera)
+        btn.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+    }
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            val image = findViewById<ImageView>(R.id.img)
+            image.setImageBitmap(imageBitmap)
+        }
+    }
+}
